b3f2e8f7c3cc1d85650f8e10b87b4de8f08fa03a
Poorna Kumar <poorna.kumar@gmail.com>
2016-05-16 21:40:16 -0700

    Minor reorganization of files

--- data_tools/make_train_val_test.py
+++ data_tools/make_train_val_test.py
@@ -1,5 +1,9 @@
 import sys
 import os
+from utils import Vocab
+import numpy as np
+import pickle
+import random
 
 """
 This file will convert the preprocessed data files into a train, val and
@@ -22,36 +26,116 @@ def get_argentina_filenames(fname):
             out.append(sp_line[1].split('=')[1])
     return out
 
-
+def write_data_to_file(read_f, write_f):
+    write_f.write(read_f.read())
+    write_f.write("<EOD>\tO\n")
+	
+    
+def clear_files():
+    for name in ['train.txt', 'dev.txt', 'test.txt']:
+	open(name, 'w').close()
 
 if __name__ == "__main__":
-    root_dir = '/media/sf_kickstarter/CS224D/Project/cs224d-project/data_tools/parsed'
+    
+
+    random.seed(42)
+    root_dir = '/media/sf_cs224d/cs224d-project/'
     # Pass the directory of the MPQA as a command line argument.
     if len(sys.argv) > 1:
         root_dir = sys.argv[1]
 
-
-    file_by_topic = '/media/sf_kickstarter/CS224D/Project/database.mpqa.1.2/Release-Full-ByTopic'
+    file_by_topic = '/media/sf_cs224d/cs224d-project/database.mpqa.1.2/Release-Full-ByTopic'
     if len(sys.argv) > 2:
         file_by_topic = sys.argv[2]
 
     argentina_filenames = get_argentina_filenames(file_by_topic)
     print len(argentina_filenames)
     print argentina_filenames
-    assert False
+
+    docs_dir = os.path.join(root_dir, 'data_tools', 'parsed')
     dir_list = os.listdir(docs_dir)
+    clear_files()
+    
     for dir_ in dir_list:
+        curr_dir = os.path.join(docs_dir, dir_)
+        file_list = os.listdir(curr_dir)
+        train_f = open('train.txt', 'a')
+	test_f = open('test.txt', 'a')
+        dev_f = open('dev.txt', 'a')
+	for fname in file_list:
+	    full_fname = os.path.join(curr_dir, fname)
+	    f = open(full_fname, 'r')
+	    if fname in argentina_filenames:
+		choice = random.choice('td')
+		if choice == 't':
+		    write_data_to_file(f, test_f)
+		else:
+		    write_data_to_file(f, dev_f)
+	    else:
+		choice = random.choice('r'*170 + 't'*15 + 'd'*15)
+		if choice == 'r':
+		    write_data_to_file(f, train_f)
+		elif choice == 't':
+		    write_data_to_file(f, test_f)
+		else:
+		    write_data_to_file(f, dev_f)
+	
+		
+  
+	    
+''''
         print 'In directory:', dir_
-        docs_subdir = os.path.join(docs_dir, dir_)
-        anns_subdir = os.path.join(anns_dir, dir_)
-        out_dir = os.path.join(out_root_dir, dir_)
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-        file_list = os.listdir(docs_subdir)
+        curr_dir = os.path.join(docs_dir, dir_)
+        file_list = os.listdir(curr_dir)
+	print curr_dir	
         for fname in file_list:
-            docs_fname = os.path.join(docs_subdir, fname)
-            with open(docs_fname, 'r') as f:
-                doc_str = f.read()
+            count_files += 1
+            full_fname = os.path.join(curr_dir, fname)
+            f = open(full_fname, 'r')
+	    for line in f:
+		sp_line = line.split()
+		word = sp_line[0]
+                all_words.add(word) #Add word to set 
+		vocab.add_word(word)
+    print "Total number of unique words (case sensitive):", len(all_words)
+    print "Total number of parsed files looked at:", count_files
+    
+    dir_for_all_words = os.path.join(root_dir, 'data_tools')
+    file_for_all_words = os.path.join(dir_for_all_words, 'all_words.txt')
+    all_words_f = open(file_for_all_words, 'w')
+    for word in all_words:
+	all_words_f.write(word + '\n')
+
+    glove_dir = os.path.join(root_dir, 'glove.840B.300d')
+    glove_f = open(os.path.join(glove_dir, 'glove.840B.300d.txt'), 'r')
+    embedding_matrix = np.zeros((len(vocab.word_to_index),300))
+    count = 0
+    for line in glove_f:
+	line_sp = line.split()
+    	word = line_sp[0]
+        line_sp_vec = [float(line_num) for line_num in line_sp[1:]] 
+        if word in vocab.word_to_index:
+	    index = vocab.word_to_index[word]
+	    embedding_matrix[index,:] = line_sp_vec
+    	    #print "Word found that matches list of words in vocab!"
+	    #print "The word is ", word
+	count += 1
+	if count%5000 == 0:
+		print "Current word:" , word
+		print count
+
+    np.save('embedding_matrix', embedding_matrix)
+    pickle.dump(vocab, open('vocab.pickle','w'))
+    	     
+
+
+
+
+
+    
+
+with open(full_fname, 'r') as f:
+	doc_str = f.read()
             anns_subsubdir = os.path.join(anns_subdir, fname)
             anns_fname = os.path.join(anns_subsubdir, 'gateman.mpqa.lre.1.2')
             with open(anns_fname, 'r') as ann_f:
@@ -75,3 +159,4 @@ if __name__ == "__main__":
                 with open(os.path.join(out_dir, fname), 'w') as out_f:
                     for out in outputs:
                         out_f.write('\t'.join([str(o) for o in out]) + '\n')
+'''
