9eb462b084d7f472a5b8d226eeb0ee8939dc7b5f
Poorna Kumar <poorna.kumar@gmail.com>
2016-05-29 19:09:05 -0700

    Pushing a bash script to run experiments on opinion mining with various architectures

---
+++ models/run_models.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python MultiRNN_bd_class.py 1 94 80
+python MultiRNN_bd_class.py 1 95 80
+python MultiRNN_bd_class.py 2 70 80
+python MultiRNN_bd_class.py 3 58 80
+python MultiRNN_bd_class.py 3 59 80
+python MultiRNN_bd_class.py 4 51 80
+python MultiRNN_bd_class.py 4 52 80
+python MultiRNN_bd_class.py 5 46 80
+python MultiRNN_bd_class.py 5 47 80
