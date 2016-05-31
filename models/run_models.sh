f46c99a10a849e26e6c4776dcdf8da797012827c
Ubuntu <ubuntu@ip-172-31-24-197.us-west-1.compute.internal>
2016-05-31 18:58:43 +0000

    Latest logs.

--- models/run_models.sh
+++ models/run_models.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python MultiRNN_bd_class.py 1 94 80 0.9 0.9 > out_1_94_80_09_09.log
-python MultiRNN_bd_class.py 2 70 80 0.9 0.9 > out_1_70_80_09_09.log
+python MultiRNN_bd_class.py 2 70 80 0.9 0.9 > out_2_70_80_09_09.log
 python MultiRNN_bd_class.py 3 58 80 0.9 0.9 > out_3_58_80_09_09.log
 python MultiRNN_bd_class.py 4 51 80 0.9 0.9 > out_4_51_80_09_09.log
 python MultiRNN_bd_class.py 5 46 80 0.9 0.9 > out_5_46_80_09_09.log
